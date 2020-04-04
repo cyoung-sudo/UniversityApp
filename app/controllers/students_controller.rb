f79cc1aaa4ee9de421bca10846ef4f1e1b99c06a
“cyoung-sudo” <cyoungcs20@gmail.com>
2020-04-04 12:06:29 -0700

    optimized redundant code

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,12 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update]
 
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
+    
   end
 
   def new
@@ -16,18 +17,17 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params)
     if @student.save
       flash[:notice] = "You have successfully signed up"
-      redirect_to root_path
+      redirect_to student_path(@student)
     else
       render 'new'
     end
   end
 
   def edit
-    @student = Student.find(params[:id])
+   
   end
 
   def update
-    @student = Student.find(params[:id])
     if @student.update(student_params)
       flash[:notice] = "Profile successfully updated"
       redirect_to student_path(@student)
@@ -39,6 +39,10 @@ class StudentsController < ApplicationController
   private
     def student_params
       params.require(:student).permit(:name, :email)
+    end
+
+    def set_student
+      @student = Student.find(params[:id])
     end
 
 end
